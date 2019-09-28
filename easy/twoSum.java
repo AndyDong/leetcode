125ded3b81236f995fc06007c0c0c0cc8de52590
Andy.Dong <827280986@qq.com>
2019-09-28 10:08:19 +0800

    力扣两数之和

---
+++ easy/twoSum.java
@@ -0,0 +1,18 @@
+class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int arrLength = nums.length;
+        int[] result = new int[2];
+        for(int i=0; i < arrLength; i++){
+            for(int j=i+1; j < arrLength; j++){
+                if (nums[i]+nums[j] != target){
+                    continue;
+                }       
+                else {
+                    result[0] = i;
+                    result[1] = j;
+                }                
+            }
+        }
+        return result;
+    }
+}
