9347cf2cecf30e3c62e3b16fb0ce46fa5b23d2e0
Andy.Dong <827280986@qq.com>
2019-09-29 23:09:17 +0800

    增加样例类

---
+++ easyExams/DemoStruct.java
@@ -0,0 +1,16 @@
+package easyExams;
+
+import java.util.Scanner;
+
+public class DemoStruct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(isPalindrome(sc.nextInt()));
+
+        }
+    }
+    private static int isPalindrome(int x) {
+        return 0;
+    }
+}
---
+++ easyExams/RomaToInt.java
@@ -0,0 +1,16 @@
+package easyExams;
+
+import java.util.Scanner;
+
+public class RomaToInt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(romanToInt(sc.nextInt()));
+
+        }
+    }
+    private static int romanToInt(int x) {
+        return 0;
+    }
+}
